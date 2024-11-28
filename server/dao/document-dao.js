af352ceeaabd3c5a01e2f27a38f9006c07750c69
Arefe <s329031@studenti.polito.it>
2024-11-28 20:10:42 +0100

    multiple stakeholder added

--- server/dao/document-dao.js
+++ server/dao/document-dao.js
@@ -11,7 +11,6 @@ const locationDao = require("./location-dao");
  *
  * @function addDocument
  * @param {String} title - Title of the document.
- * @param {Number} idStakeholder - Unique identifier of the stakeholder associated with the document.
  * @param {String} scale - The scale of the document (e.g., 'National', 'Regional').
  * @param {String} issuance_Date - The issuance date of the document in 'MM/DD/YYYY' format.
  * @param {String} language - Language of the document (e.g., 'English').
@@ -21,18 +20,47 @@ const locationDao = require("./location-dao");
  * @param {Number} idlocation - The location of a specific document
  * @returns {Promise<Number>} Resolves with newly object.
  */
-exports.addDocument = async (title,idStakeholder,scale,issuance_Date,language,pages,description,idtype,idlocation) => {
+exports.addDocument = async (
+  title,
+  scale,
+  issuance_Date,
+  language,
+  pages,
+  description,
+  idtype,
+  idlocation
+) => {
   return new Promise((resolve, reject) => {
     const sql =
-      "INSERT INTO Document (Title, IdStakeholder, IdScale, Issuance_Date, Language, Pages, Description, IdType, IdLocation) VALUES (?,?,?,?,?,?,?,?,?)";
+      "INSERT INTO Document (Title, IdScale, Issuance_Date, Language, Pages, Description, IdType, IdLocation) VALUES (?,?,?,?,?,?,?,?)";
     db.run(
-      sql,[title,idStakeholder,scale,issuance_Date,language,pages,description,idtype,idlocation],
+      sql,
+      [
+        title,
+        scale,
+        issuance_Date,
+        language,
+        pages,
+        description,
+        idtype,
+        idlocation,
+      ],
       function (err) {
         if (err) {
           reject(err);
           return;
         }
-        const newdocument = new Document(this.lastID,title,idStakeholder,scale,issuance_Date,language,pages,description,idtype,idlocation);
+        const newdocument = new Document(
+          this.lastID,
+          title,
+          scale,
+          issuance_Date,
+          language,
+          pages,
+          description,
+          idtype,
+          idlocation
+        );
         resolve(newdocument);
       }
     );
@@ -40,58 +68,80 @@ exports.addDocument = async (title,idStakeholder,scale,issuance_Date,language,pa
 };
 // here other function es get document
 exports.getDocuments = () => {
-    return new Promise((resolve, reject) => {
-        const sql = 'SELECT * FROM Document';
+  return new Promise((resolve, reject) => {
+    const sql = "SELECT * FROM Document";
 
-        db.all(sql, (err, row) => {
-            if (err) {
-                reject(err);
-            } else {
-                resolve(row)
-                };
-            })
+    db.all(sql, (err, row) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(row);
+      }
     });
+  });
 };
 exports.getDocumentById = (documentId) => {
-    return new Promise((resolve, reject) => {
-        const sql = 'SELECT * FROM Document WHERE IdDocument = ?';
+  return new Promise((resolve, reject) => {
+    const sql = "SELECT * FROM Document WHERE IdDocument = ?";
 
-        db.get(sql, [documentId], (err, row) => {
-            if (err) {
-                reject(err);
-            } else {
-                resolve(row)
-            };
-            }) 
-        });
-    };
+    db.get(sql, [documentId], (err, row) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(row);
+      }
+    });
+  });
+};
 exports.getDocumentByTitle = (title) => {
-      return new Promise((resolve, reject) => {
-          const sql = 'SELECT * FROM Document WHERE Title = ?';
-          db.get(sql, [title], (err, row) => {
-              if (err) {
-                  reject(err);
-              } else {
-                  resolve(row);
-              }
-          });
-      });
-  };  
+  return new Promise((resolve, reject) => {
+    const sql = "SELECT * FROM Document WHERE Title = ?";
+    db.get(sql, [title], (err, row) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(row);
+      }
+    });
+  });
+};
 /**
  * Updating the document id of a document.
  * @param {Number} documentId - ID of the document to update.
  * @returns {Promise<Boolean>} resolved with the new object update.
  */
-exports.updateDocument = (documentId, title,idStakeholder,scale,issuance_Date,language,pages,description,idtype) => { 
-    return new Promise((resolve, reject) => { 
-      const sql = "UPDATE Document SET Title = ?, IdStakeholder = ?, IdScale = ?, Issuance_Date = ?, Language = ?, Pages = ?, Description = ?, IdType = ?  WHERE IdDocument = ?"; 
-      db.run(sql, [title,idStakeholder,scale,issuance_Date,language,pages,description,idtype,documentId], function (err) { 
-        if (err) { 
-          reject(new Error("Failed to update document.")); 
-          return; 
-        } 
-        resolve(true); 
-      }); 
-})};
-
-
+exports.updateDocument = (
+  documentId,
+  title,
+  scale,
+  issuance_Date,
+  language,
+  pages,
+  description,
+  idtype
+) => {
+  return new Promise((resolve, reject) => {
+    const sql =
+      "UPDATE Document SET Title = ?, IdScale = ?, Issuance_Date = ?, Language = ?, Pages = ?, Description = ?, IdType = ?  WHERE IdDocument = ?";
+    db.run(
+      sql,
+      [
+        title,
+        scale,
+        issuance_Date,
+        language,
+        pages,
+        description,
+        idtype,
+        documentId,
+      ],
+      function (err) {
+        if (err) {
+          reject(new Error("Failed to update document."));
+          return;
+        }
+        resolve(true);
+      }
+    );
+  });
+};
